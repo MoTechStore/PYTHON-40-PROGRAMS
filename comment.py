a76cb82a9568b14852dc3fe8de015af5a8e70a10
MoTech <53371699+MoTechStore@users.noreply.github.com>
2019-10-03 06:50:03 -0700

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,15 @@
+print("This program show how to comment in python : ")
+# This is single line comment
+"""
+
+This
+is
+multiple
+line
+comment
+
+"""
+print("Single And Multiple line comment will not display : ")
+
+
+
