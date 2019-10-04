ec4f9913db5c3e470197c08d79bf523c380486f9
MoTech <53371699+MoTechStore@users.noreply.github.com>
2019-10-03 17:07:01 -0700

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,8 @@
+country = input("Which Country You From : ")
+city = input("Which City You From : ")
+print("So You Come From ", country," And City Called ",city)
+input("Press Any Key To Quit : ")
+
+
+
+
