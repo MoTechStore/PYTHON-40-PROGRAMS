921a26ef4c4725b232ea0703adb598c750c145b1
MoTech <53371699+MoTechStore@users.noreply.github.com>
2019-10-03 17:45:50 -0700

    Add files via upload

---
+++ multiple_userinput.py
@@ -0,0 +1,12 @@
+print("ProgramTo Get Multiple User Input : ")
+firstname,middlename,lastname = input("Enter Your Three Names : ").split()
+
+print("Your First Name Is : ",firstname)
+print("Your Middle Name Is : ",middlename)
+print("Your Last Name Is : ",lastname)
+
+input("Press Any Key To Quit : ")
+
+
+
+
