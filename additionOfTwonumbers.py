bddac8f19e57eacb942aa2eaa117401eb25b72d7
MoTech <53371699+MoTechStore@users.noreply.github.com>
2019-10-03 17:28:44 -0700

    Add files via upload

---
+++ additionOfTwonumbers.py
@@ -0,0 +1,10 @@
+print("Program To Add Two Numbers : ")
+numberone = int(input("Enter Number One : "))
+numbertwo = int(input("Enter Number Two : "))
+
+total = numberone + numbertwo
+print("Addition of Two Number is : ",total)
+
+input("Press Any Key To Quit : ")
+
+
